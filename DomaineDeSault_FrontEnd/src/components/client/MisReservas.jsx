b371e1d293774d2c863c2449df0148c5b10aefcb
Eduardito <grupohemsa@gmail.com>
2026-03-12 20:35:37 +0100

    Feat: Se crea la view de cliente autenticado y pagina basica de mis reservas. (#20)

---
+++ DomaineDeSault_FrontEnd/src/components/client/MisReservas.jsx
@@ -0,0 +1,10 @@
+const MisReservas = () => {
+  return (
+    <div className="container" style={{ marginTop: "120px" }}>
+      <h2>Mis reservas</h2>
+      <p>Aquí verás tus reservas pendientes y confirmadas.</p>
+    </div>
+  );
+};
+
+export default MisReservas;
